dc4bffaae61f64ea1d633319567e341fe8cf4271
Martin Phu <extramart@outlook.com>
2024-02-21 11:50:58 +0100

    first commit

---
+++ src/main/java/com/project/stilgalleriet/StilgallerietApplication.java
@@ -0,0 +1,13 @@
+package com.project.stilgalleriet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StilgallerietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StilgallerietApplication.class, args);
+	}
+
+}
