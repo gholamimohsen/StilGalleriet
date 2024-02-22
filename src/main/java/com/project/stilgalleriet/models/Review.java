06a46cb00cb94b6d88c855ca8cb0031073853583
Martin Phu <extramart@outlook.com>
2024-02-22 22:58:07 +0100

    Finished Review model

--- src/main/java/com/project/stilgalleriet/models/Review.java
+++ src/main/java/com/project/stilgalleriet/models/Review.java
@@ -1,4 +1,59 @@
 package com.project.stilgalleriet.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
 public class Review {
+
+    @Id
+    private String id;
+
+    private String ratingUserId; //Add @DBRef after other CRUD are done
+
+    private String ratedUserId; //Add @DBRef after other CRUD are done
+
+    private String rating; //Might need to make an enum for this field
+
+    private String comment;
+
+    public Review(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getRatingUserId() {
+        return ratingUserId;
+    }
+
+    public void setRatingUserId(String ratingUserId) {
+        this.ratingUserId = ratingUserId;
+    }
+
+    public String getRatedUserId() {
+        return ratedUserId;
+    }
+
+    public void setRatedUserId(String ratedUserId) {
+        this.ratedUserId = ratedUserId;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
