01e4fcdd4cd376a1f2388307eb56f5a1593fa4f0
Fazilet1820 <senolfazilet1820@gmail.com>
2024-02-22 10:31:33 +0100

    test commit

--- src/main/java/com/project/stilgalleriet/models/Advertisement.java
+++ src/main/java/com/project/stilgalleriet/models/Advertisement.java
@@ -1,4 +1,8 @@
 package com.project.stilgalleriet.models;
 
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="advertisement")
 public class Advertisement {
 }
