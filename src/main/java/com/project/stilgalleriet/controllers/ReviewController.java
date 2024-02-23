5039422f22206047216195e8e1804d0f20d49bad
Martin Phu <extramart@outlook.com>
2024-02-23 20:47:57 +0100

    Added create to reviews

--- src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
+++ src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
@@ -1,4 +1,25 @@
 package com.project.stilgalleriet.controllers;
 
+import com.project.stilgalleriet.models.Review;
+import com.project.stilgalleriet.services.ReviewService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stilgalleriet/reviews")
 public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> addReview(@Valid @RequestBody Review review){
+        Review newReview = reviewService.addReview(review); //Change to take user IDs from orders and put it in Review object
+        return new ResponseEntity<>(newReview, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/project/stilgalleriet/services/ReviewService.java
+++ src/main/java/com/project/stilgalleriet/services/ReviewService.java
@@ -1,4 +1,18 @@
 package com.project.stilgalleriet.services;
 
+import com.project.stilgalleriet.models.Review;
+import com.project.stilgalleriet.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    //Create a new review
+    public Review addReview(Review review){
+        return reviewRepository.save(review);
+    }
+
 }
