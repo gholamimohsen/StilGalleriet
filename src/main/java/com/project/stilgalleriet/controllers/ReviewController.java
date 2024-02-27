ee21514ccf396a9ff665af9def5d42a07724e46e
Martin Phu <extramart@outlook.com>
2024-02-27 13:36:42 +0100

    Basic CRUD done

--- src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
+++ src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stilgalleriet/reviews")
@@ -21,5 +20,18 @@ public class ReviewController {
     public ResponseEntity<Review> addReview(@Valid @RequestBody Review review){
         Review newReview = reviewService.addReview(review); //Change to take user IDs from orders and put it in Review object
         return new ResponseEntity<>(newReview, HttpStatus.CREATED);
+    }
+
+    //Maybe remove this later, check comment in ReviewService
+    @GetMapping("/all")
+    public List<Review> getAllReviews(){
+        return reviewService.getAllReviews();
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> updateReview(@PathVariable String id, @Valid @RequestBody Review review){
+
+        Review updatedReview = reviewService.updateReview(id, review);
+        return ResponseEntity.ok(updatedReview);
     }
 }
--- src/main/java/com/project/stilgalleriet/services/ReviewService.java
+++ src/main/java/com/project/stilgalleriet/services/ReviewService.java
@@ -5,6 +5,8 @@ import com.project.stilgalleriet.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewService {
     @Autowired
@@ -13,6 +15,33 @@ public class ReviewService {
     //Create a new review
     public Review addReview(Review review){
         return reviewRepository.save(review);
+    }
+
+    //Get all reviews, can have it for testing CRUD not sure if needed later on
+    public List<Review> getAllReviews(){
+        return reviewRepository.findAll();
+    }
+
+    //Get reviews by seller user ID, implemented later as Order need to be functional
+
+    //Update a review
+    public Review updateReview(String id, Review updatedReview){
+        return reviewRepository.findById(id)
+                .map(review -> {
+                    if (updatedReview.getRating() != null){
+                        review.setRating(updatedReview.getRating());
+                    }
+                    if (updatedReview.getComment() != null){
+                        review.setComment(updatedReview.getComment());
+                    }
+                    return reviewRepository.save(review);
+                })
+                .orElseThrow(); //Add exception handling
+    }
+
+    //Delete review
+    public void deleteReview(String id){
+        reviewRepository.deleteById(id);
     }
 
 }
