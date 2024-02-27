b5bedbb343015b5ab4cb43f053f1be14e342683b
Mohsen Gholami <gholamimohsen1999@gmail.com>
2024-02-27 15:39:36 +0100

    Test

--- src/main/java/com/project/stilgalleriet/models/User.java
+++ src/main/java/com/project/stilgalleriet/models/User.java
@@ -1,4 +1,24 @@
 package com.project.stilgalleriet.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+
+@Document(collection = "User")
 public class User {
+   @Id
+   private String id;
+   private String firstName;
+   private String lastName;
+   private String email;
+   private String password;
+   private ArrayList adress;
+   private Enum role;
+   private boolean isActive;
+   private ArrayList favorites;
+   private String advertisementId;
+
+
+
 }
