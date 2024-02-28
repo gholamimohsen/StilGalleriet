aa9f6a485a22cdb322cbec2b50906eb8df052ec5
Dany Ramirez <dany.ramirez.92@hotmail.com>
2024-02-28 18:25:54 +0100

    Created a AuthController class

---
+++ src/main/java/com/project/stilgalleriet/controllers/AuthController.java
@@ -0,0 +1,9 @@
+package com.project.stilgalleriet.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AuthController {
+}
