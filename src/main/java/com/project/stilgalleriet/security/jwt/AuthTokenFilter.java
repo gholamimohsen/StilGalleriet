f89cbbb573a4cfb5691cab15e0081c87e527be54
German Fuentes <s.g.f.v@hotmail.se>
2024-02-28 16:23:55 +0100

    created security package

---
+++ src/main/java/com/project/stilgalleriet/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,4 @@
+package com.project.stilgalleriet.security.jwt;
+
+public class AuthTokenFilter {
+}
---
+++ src/main/java/com/project/stilgalleriet/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,4 @@
+package com.project.stilgalleriet.security.services;
+
+public class UserDetailsServiceImpl {
+}
