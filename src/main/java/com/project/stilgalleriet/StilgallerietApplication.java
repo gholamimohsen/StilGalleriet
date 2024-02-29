63bb65105bb316e90d924886ba358ad6e7416443
Ilham F <ilham938472@gmail.com>
2024-02-29 18:50:03 +0100

    Add constructors, getters, and setters to Order class

--- src/main/java/com/project/stilgalleriet/StilgallerietApplication.java
+++ src/main/java/com/project/stilgalleriet/StilgallerietApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StilgallerietApplication {
+public class StilGallerietApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StilgallerietApplication.class, args);
+		SpringApplication.run(StilGallerietApplication.class, args);
 	}
 
 }
