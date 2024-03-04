4d35bf063cdfd2e12b6e818bce214e9df45d5c21
Mohsen Gholami <gholamimohsen1999@gmail.com>
2024-03-04 10:20:29 +0100

    Updated the code

--- src/main/java/com/project/stilgalleriet/controllers/UserController.java
+++ src/main/java/com/project/stilgalleriet/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/stilgalleriet/users")
 
 public class UserController {
     @Autowired
