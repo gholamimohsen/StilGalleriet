6632f136370197be42ba550b56d66e08e95eaf02
Mohsen Gholami <gholamimohsen1999@gmail.com>
2024-03-04 19:35:10 +0100

    Updated User model

--- src/main/java/com/project/stilgalleriet/models/User.java
+++ src/main/java/com/project/stilgalleriet/models/User.java
@@ -1,111 +1,147 @@
 package com.project.stilgalleriet.models;
 
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.management.relation.Role;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 
 @Document(collection = "users")
 public class User {
-   @Id
-   private String id;
-   @DBRef
-   private Set<Role> roles = new HashSet<>();
-   private String firstName;
-   private String lastName;
-   private String email;
-   private String password;
-   private ArrayList address;
-   private Enum role;
-   private boolean isActive;
-   private ArrayList favorites;
-   private String advertisementId;
+
+    @Id
+    private String id;
+
+    /*@DBRef
+    private Set<Role> roles = new HashSet<>();*/
+
+    private String firstName;
+
+    private String lastName;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String address;
+
+    private Enum role;
+
+    @CreatedDate
+    private Date createdAt;
+
+    private boolean isActive = true;
+
+    private ArrayList favorites;
+
+    private String advertisementId;
 
 
-   public User() {
-   }
 
-   public String getId() {
-      return id;
-   }
+    public User() {
+    }
 
-   public void setId(String id) {
-      this.id = id;
-   }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getId() {
+        return id;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    /*public Set<Role> getRoles() {
+        return roles;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }*/
 
-   public String getEmail() {
-      return email;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getPassword() {
-      return password;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setPassword(String password) {
-      this.password = password;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public ArrayList getAddress() {
-      return address;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public void setAddress(ArrayList adress) {
-      this.address = adress;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public Enum getRole() {
-      return role;
-   }
+    public String getPassword() {
+        return password;
+    }
 
-   public void setRole(Enum role) {
-      this.role = role;
-   }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-   public boolean isActive() {
-      return isActive;
-   }
+    public String getAddress() {
+        return address;
+    }
 
-   public void setActive(boolean active) {
-      isActive = active;
-   }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-   public ArrayList getFavorites() {
-      return favorites;
-   }
+    public Enum getRole() {
+        return role;
+    }
 
-   public void setFavorites(ArrayList favorites) {
-      this.favorites = favorites;
-   }
+    public void setRole(Enum role) {
+        this.role = role;
+    }
 
-   public String getAdvertisementId() {
-      return advertisementId;
-   }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 
-   public void setAdvertisementId(String advertisementId) {
-      this.advertisementId = advertisementId;
-   }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public ArrayList getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(ArrayList favorites) {
+        this.favorites = favorites;
+    }
+
+    public String getAdvertisementId() {
+        return advertisementId;
+    }
+
+    public void setAdvertisementId(String advertisementId) {
+        this.advertisementId = advertisementId;
+    }
 }
+
+
+
