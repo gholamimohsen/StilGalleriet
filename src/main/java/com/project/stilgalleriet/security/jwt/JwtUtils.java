06f93dc0279d1423445b5148aa9f62de3a082cad
Dany Ramirez <dany.ramirez.92@hotmail.com>
2024-03-08 15:53:15 +0100

    changed code in authTokenFilter

--- src/main/java/com/project/stilgalleriet/security/jwt/JwtUtils.java
+++ src/main/java/com/project/stilgalleriet/security/jwt/JwtUtils.java
@@ -1,8 +1,7 @@
 package com.project.stilgalleriet.security.jwt;
 
 import com.project.stilgalleriet.security.services.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
@@ -45,7 +44,23 @@ public class JwtUtils { //En hjälp class (hjälpmetoder som hanterar tokens)
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key()).build()
                 .parseClaimsJws(token).getBody().getSubject();
-
-
     }
+
+    //validera token med secret
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token; {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token has expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claim string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
 }
