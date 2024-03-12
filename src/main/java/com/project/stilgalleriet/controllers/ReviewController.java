1ea628e994899cbd8aceb9da5ca48292d8f4bdd3
Martin Phu <extramart@outlook.com>
2024-03-12 15:24:59 +0100

    Changed path of RequestMapping

--- src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
+++ src/main/java/com/project/stilgalleriet/controllers/ReviewController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/stilgalleriet/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
