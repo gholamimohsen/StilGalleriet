38b6b2fb3fc6940e98d2d9097a0cbaeaf997d8f9
Martin Phu <extramart@outlook.com>
2024-03-20 21:26:59 +0100

    Added annotations to DTO

--- src/main/java/com/project/stilgalleriet/dto/ReviewDTO.java
+++ src/main/java/com/project/stilgalleriet/dto/ReviewDTO.java
@@ -2,15 +2,18 @@ package com.project.stilgalleriet.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 public class ReviewDTO { //DTO for Reviews. Used for both request and response.
+    @NotBlank(message = "Can't be found")
     private String ratingUserId; //Can grab this ID somehow with authentication or being logged in(user account)?
-
+    @NotBlank(message = "Can't be found")
     private String ratedUserId;
 
     @Min(value = 1, message = "Minimum is 1") @Max(value = 10, message = "10 is the highest")
     private int rating;
-
+    @Length(min = 0, max = 200, message = "You exceeded 200 letters")
     private String comment;
 
     public String getRatingUserId() {
