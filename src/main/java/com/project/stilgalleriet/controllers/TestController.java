2769af2e8864f9628578edfefba48fb8c3b090a2
Fazilet1820 <senolfazilet1820@gmail.com>
2024-05-18 23:27:33 +0200

    @CrossOrigin annotation added to all controller classes

--- src/main/java/com/project/stilgalleriet/controllers/TestController.java
+++ src/main/java/com/project/stilgalleriet/controllers/TestController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
 @RequestMapping("/api/test")
 public class TestController {
 
--- src/main/java/com/project/stilgalleriet/controllers/UserController.java
+++ src/main/java/com/project/stilgalleriet/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 
