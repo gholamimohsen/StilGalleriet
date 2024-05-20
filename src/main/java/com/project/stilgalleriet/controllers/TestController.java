b02e58a73a47b88d35762db3f3e3731f6993681b
Fazilet1820 <senolfazilet1820@gmail.com>
2024-05-20 19:00:36 +0200

    @CrossOrigin anotation has commited because project does not start properly

--- src/main/java/com/project/stilgalleriet/controllers/TestController.java
+++ src/main/java/com/project/stilgalleriet/controllers/TestController.java
@@ -3,13 +3,12 @@ package com.project.stilgalleriet.controllers;
 import com.project.stilgalleriet.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/test")
 public class TestController {
 
