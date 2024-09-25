1c81c2e52e7c3535312d8c9e7e68447db145b656
German Fuentes <s.g.f.v@hotmail.se>
2024-09-25 17:59:52 +0200

    Skapat två exception klasser med tillhörande custom metoder

---
+++ src/main/java/com/project/stilgalleriet/exception/AdvertisementNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.stilgalleriet.exception;
+
+public class AdvertisementNotFoundException extends RuntimeException {
+    public AdvertisementNotFoundException(String message) {
+
+    }
+}
