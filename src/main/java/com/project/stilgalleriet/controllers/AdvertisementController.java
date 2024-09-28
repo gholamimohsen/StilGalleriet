bdd1908fce0a51463c0c2b848a8b4f9f89332d9c
Fazilet1820 <senolfazilet1820@gmail.com>
2024-09-28 10:13:40 +0200

    old methods did not deleted.Because the news have not been tested yet.

--- src/main/java/com/project/stilgalleriet/controllers/AdvertisementController.java
+++ src/main/java/com/project/stilgalleriet/controllers/AdvertisementController.java
@@ -63,50 +63,7 @@ public class AdvertisementController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
-
-
-    /*@GetMapping("/filter") // new endpoint for all filter functions --Strategi Pattern--
-    public List<Advertisement> filterAdvertisements(@RequestParam("filterType") String filterType,
-                                                    @RequestParam Map<String, String> params) {
-        AdvertisementFilterStrategy strategy = getFilterStrategy(filterType, params);
-        return strategy.filterAdvertisements(advertisementService);
-    }
-    private AdvertisementFilterStrategy getFilterStrategy (String filterType, Map<String, String> params){
-        switch (filterType){
-            case "color":
-            String color=params.get("color");
-            return new AdFilterByColor(color);
-            case "gender":
-                String gender=params.get("gender");
-                return new AdFilterByGender(gender);
-            case "size":
-                String size=params.get("size");
-                return new AdFilterBySize(size);
-            case "category":
-                String category=params.get("category");
-                return new AdFilterByCategory(category);
-            case "priceGreaterThan":
-                double minPrice=Double.parseDouble(params.get("minPrice"));
-                return new AdFilterByMinPrice(minPrice);
-            case "priceBetween":
-                double minPrice2=Double.parseDouble(params.get("minPrice"));
-                double maxPrice2=Double.parseDouble(params.get("maxPrice2"));
-                return new AdFilterByPriceBetween(minPrice2, maxPrice2);
-            case "priceLessThan":
-                double maxPrice=Double.parseDouble(params.get("maxPrice"));
-                return new AdFilterByMaxPrice(maxPrice);
-            case "createdBefore":
-                String dateStringBefore = params.get("date");
-                return new AdFilterByDateBefore(dateStringBefore);
-            case "createdAfter":
-                String dateStringAfter=params.get("date");
-                return new AdFilterByDateAfter(dateStringAfter);
-            default:
-                throw new IllegalArgumentException("Unknown filter type");
-
-        }
-    }*/
-    // Old seperate methods. But all of them run
+    // Old seperate methods. But all of them run. Because I couldn't test the new one, I didn't delete them.
 
     /*@GetMapping("/color/{color}")
 
